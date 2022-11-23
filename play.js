28198a3ecb9f6034ac23ce0bcbac61a4aa01be64
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-23 15:24:00 -0500

    setup input to exit with Ctl C:
    .

--- play.js
+++ play.js
@@ -1,23 +1,16 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: "50541"
-  });
-
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on('data', (data) => {
-    console.log('data from server: ', data);
-  })
-
-  return conn;
-
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
 };
+// setup interface to handle user input from stdin
 
-console.log("Connecting ...");
-connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
